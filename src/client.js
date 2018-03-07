e51e4dcbe5a77da57a6c373552ed368aaee7123e
kazuma1989 <funifuni.1204@gmail.com>
2018-03-07 21:25:48 +0900

    expt: Use prh package directly.

--- src/client.js
+++ src/client.js
@@ -1,8 +1,22 @@
+// import { exec } from 'child_process';
+import { readFileSync } from 'fs';
 import { remote } from 'electron';
+import { fromYAMLFilePaths, getRuleFilePath } from 'prh';
 
 const { BrowserWindow, dialog } = remote;
 
 function openLoadFile() {
+  // exec('./resources/app/node_modules/prh/bin/prh --rules C:\\Users\\kebina1\\workspace\\prh-app\\src\\prh.yml --diff C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc', (err, stdout, stderr) => {
+  //   if (err) { console.log(err); }
+  //   console.log(stdout);
+  // });
+
+  const engine = fromYAMLFilePaths(getRuleFilePath('C:\\Users\\kebina1\\workspace\\prh-app\\src'))
+  const changeSet = engine.makeChangeSet('C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc');
+  const content = readFileSync('C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc', { encoding: 'utf8' });
+  const result = changeSet.applyChangeSets(content);
+  console.log(result);
+
   const modalTarget = BrowserWindow.getFocusedWindow();
 
   dialog.showOpenDialog(
@@ -16,13 +30,13 @@ function openLoadFile() {
         }
       ]
     },
-    filenames => {
-      if (!filenames) {
+    filePaths => {
+      if (!filePaths) {
         return;
       }
 
-      filenames.forEach(filename => {
-        console.log(filename);
+      filePaths.forEach(path => {
+        console.log(path);
       });
     }
   );
