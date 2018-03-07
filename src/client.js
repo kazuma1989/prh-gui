5b0a076fb71febe3e0e48756d2faed8c33ba0c09
kazuma1989 <funifuni.1204@gmail.com>
2018-03-07 20:26:20 +0900

    fix: Multiple files selectable.

--- src/client.js
+++ src/client.js
@@ -2,49 +2,28 @@ import { remote } from 'electron';
 
 const { BrowserWindow, dialog } = remote;
 
-/**
- * 読み込みするためのファイルを開く
- */
 function openLoadFile() {
-  const win = BrowserWindow.getFocusedWindow();
+  const modalTarget = BrowserWindow.getFocusedWindow();
 
   dialog.showOpenDialog(
-    win,
-    // どんなダイアログを出すかを指定するプロパティ
+    modalTarget,
     {
-      properties: ['openFile'],
+      properties: ['openFile', 'multiSelections'],
       filters: [
         {
           name: 'Documents',
-          extensions: ['txt', 'text', 'html', 'js', 'adoc']
+          extensions: ['txt', 'md', 'html', 'css', 'js', 'adoc', 'json', 'yaml', 'yml', 'csv', 'tsv']
         }
       ]
     },
-    // [ファイル選択]ダイアログが閉じられた後のコールバック関数
-    function (filenames) {
-      if (filenames) {
-        readFile(filenames[0]);
+    filenames => {
+      if (!filenames) {
+        return;
       }
-    });
-}
 
-import fs from 'fs';
-/**
- * テキストを読み込み、テキストを入力エリアに設定する
- */
-function readFile(path) {
-  const footerArea = document.querySelector('footer');
-  const editor = document.querySelector('#editor');
-  // currentPath = path;
-  fs.readFile(path, function (error, text) {
-    if (error != null) {
-      alert('error : ' + error);
-      return;
+      filenames.forEach(filename => {
+        console.log(filename);
+      });
     }
-    // フッター部分に読み込み先のパスを設定する
-    footerArea.innerHTML = path;
-    // テキスト入力エリアに設定する
-    // editor.setValue(text.toString(), -1);
-    editor.textContent = text.toString();
-  });
+  );
 }
