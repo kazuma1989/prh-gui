057c512fbb7686f7ad5a630ed80d70dbd4779551
kazuma1989 <funifuni.1204@gmail.com>
2018-03-08 14:36:28 +0900

    refactor: Add styles

--- src/client.js
+++ src/client.js
@@ -1,22 +1,14 @@
-// import { exec } from 'child_process';
-// import { readFileSync } from 'fs';
 import { remote } from 'electron';
-// import { fromYAMLFilePaths, getRuleFilePath } from 'prh';
+import { applyBindings } from 'knockout';
 import prhCliImitate from './prh-cli-imitate';
+
+applyBindings({
+  openFiles
+});
 
 const { BrowserWindow, dialog } = remote;
 
-function openLoadFile() {
-  // exec('./resources/app/node_modules/prh/bin/prh --rules C:\\Users\\kebina1\\workspace\\prh-app\\src\\prh.yml --diff C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc', (err, stdout, stderr) => {
-  //   if (err) { console.log(err); }
-  //   console.log(stdout);
-  // });
-
-  // const engine = fromYAMLFilePaths(getRuleFilePath('C:\\Users\\kebina1\\workspace\\prh-app\\src'))
-  // const changeSet = engine.makeChangeSet('C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc');
-  // const content = readFileSync('C:\\Users\\kebina1\\workspace\\prh-app\\src\\test.adoc', { encoding: 'utf8' });
-  // const result = changeSet.applyChangeSets(content);
-  // console.log(result);
+function openFiles() {
 
   const modalTarget = BrowserWindow.getFocusedWindow();
 
@@ -46,7 +38,7 @@ function openLoadFile() {
       };
       const { result, invalidFiles } = prhCliImitate(filePaths, options);
 
-      result.forEach(({filePath, changeSet}) => {
+      result.forEach(({ filePath, changeSet }) => {
         console.log(filePath);
         changeSet.forEach(console.log);
       });
